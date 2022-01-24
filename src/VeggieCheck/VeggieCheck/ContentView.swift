6354849b5c4219db6f67fef46d81fb3bdc51cfcf
grigora2 <amelia.grigoriev2@mail.dcu.ie>
2022-01-24 18:50:23 +0000

    creating VeggieCheck folder

---
+++ src/VeggieCheck/VeggieCheck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VeggieCheck
+//
+//  Created by Amelia Grigoriev on 24/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ src/VeggieCheck/VeggieCheck/VeggieCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeggieCheckApp.swift
+//  VeggieCheck
+//
+//  Created by Amelia Grigoriev on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct VeggieCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
