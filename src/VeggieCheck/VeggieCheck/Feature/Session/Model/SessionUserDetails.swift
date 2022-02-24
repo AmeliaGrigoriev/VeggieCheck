61ad4640c8c0f566aa0ae0b54c150b851cd1dc94
grigora2 <amelia.grigoriev2@mail.dcu.ie>
2022-02-24 14:22:40 +0000

    Created sessions

---
+++ src/VeggieCheck/VeggieCheck/Feature/Session/Model/SessionUserDetails.swift
@@ -0,0 +1,13 @@
+//
+//  SessionUserDetails.swift
+//  VeggieCheckTesting
+//
+//  Created by Amelia Grigoriev on 20/02/2022.
+//
+
+import Foundation
+
+struct SessionUserDetails {
+    let firstName: String
+    let lastName: String
+}
