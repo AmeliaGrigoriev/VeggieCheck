1f306e424da552c2698ff315bdc020a368165a6c
grigora2 <amelia.grigoriev2@mail.dcu.ie>
2022-02-24 13:33:06 +0000

    Added to login view

--- src/VeggieCheck/VeggieCheck/Feature/Login/View/LoginView.swift
+++ src/VeggieCheck/VeggieCheck/Feature/Login/View/LoginView.swift
@@ -9,9 +9,51 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            VStack(spacing: 16) {
+                
+                VStack(spacing: 16) {
+                    
+                    InputTextFieldView(text: .constant(""),
+                                       placeholder: "Email",
+                                       keyboardType: .emailAddress,
+                                       sfSymbol: "envelope")
+                    
+                    InputPasswordView(password: .constant(""),
+                                      placeholder: "Password",
+                                      sfSymbol: "lock")
+                }
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // TODO: Handle presentation to forgot password
+                    }, label: {
+                        Text("Forgot Password?")
+                            .foregroundColor(Color.green)
+                    })
+                    .font(.system(size: 16, weight: .bold))
+                }
+                
+                VStack(spacing: 16) {
+                    
+                    ButtonView(title: "Login") {
+                        // TODO: Handle login action here
+                    }
+                    
+                    ButtonView(title: "Register",
+                               background: .clear,
+                               foreground: .green,
+                               border: .green) {
+                        // TODO: Handle presentation to registation
+                    }
+                }
+            }
+            .padding(.horizontal, 15)
+            .navigationTitle("Login")
     }
 }
+
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
